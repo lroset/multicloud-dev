7e3e036eb82edef6866ff33dfe3103a3cb648eb4
lroset <162152798+lroset@users.noreply.github.com>
2024-03-13 07:42:35 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,19 +2,24 @@
 
 variable "s3-name" {
     description = "name of the s3 to be created"
+    default = "test"
 }
 variable "s3-prefix" {
     description = "S3 prefix"
+    default = "test"
 }
 variable "aws-region"{
     description = "region to deploy"
+    default = "us-east-2"
 }
 
 // Azure variables
 
 variable "rg-name" {
-  description = "resource group name"
+    description = "resource group name"
+    default = rg-example"
 }
-variable "az-region" {
-  description = "resource group region"
+variable "az-region" {    
+    description = "resource group region"
+    default = "East US"
 }
