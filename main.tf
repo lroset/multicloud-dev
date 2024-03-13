fef2ae9bd19415931384ce694612d3733c06157d
lroset <162152798+lroset@users.noreply.github.com>
2024-03-13 09:15:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ module "resource_group" {
 // AWS Private Registry Module
 module "s3_webapp" {
   source  = "app.terraform.io/ACME-Luis/s3-webapp/aws"
+  version = "1.0.3"
 
   name = "multicloud"
   prefix = "s3"
