094f2778833a0b2ea07dae6edb7179e4b185cbb6
lroset <162152798+lroset@users.noreply.github.com>
2024-03-13 08:21:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "resource_group" {
 module "s3_webapp" {
   source  = "app.terraform.io/ACME-Luis/s3-webapp/aws"
 
-  name = "cloud"
+  name = "cloud2"
   prefix = "multi"
   region = "us-east-2"
 }
