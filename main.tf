492c0622ce4a22576596625450ced728e121cbba
lroset <162152798+lroset@users.noreply.github.com>
2024-03-13 08:55:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "resource_group" {
 module "s3_webapp" {
   source  = "app.terraform.io/ACME-Luis/s3-webapp/aws"
 
-  name = "nulticloud"
+  name = "multicloud"
   prefix = "s3"
   region = "us-east-2"
 }
